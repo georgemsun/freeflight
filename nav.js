57d6ee54492b35059795628a052ba96c32988787
George Sun <gmsun27@gmail.com>
2015-07-25 13:38:12 -0700

    Updated nav.js

--- nav.js
+++ nav.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
+	var fadeOut = 1;
+	var fadeIn = 900;
 	var resetNav = function() {
-		$('.home, .indoor-duration, .olympiad, .utilities').css('display', 'none');
+		$('.home, .indoor-duration, .olympiad, .utilities').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'none'); }).fadeTo(fadeOut, 1);
 		$('.nav > li > a:link, .nav > li > la:visited').css('color', 'grey');
 	};
 	resetNav();
-	$('.home').css('display', 'block');
-	$('.nav-home').css('color', 'black');
+		$('.home').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'block'); }).fadeTo(fadeIn, 1);
+		$('.nav-home').css('color', 'black');
 	$('title').html("Free Flight");
 	$('.nav-home').click(function() {
 		resetNav();
-		$('.home').css('display', 'block');
+		$('.home').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'block'); }).fadeTo(fadeIn, 1);
 		$('.nav-home').css('color', 'black');
 		$('title').html("Free Flight");
 	});
 	$('.nav-indoor-duration, .nav-indoor-articles, .nav-indoor-photos').click(function() {
 		resetNav();
-		$('.indoor-duration').css('display', 'block');
+		$('.indoor-duration').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'block'); }).fadeTo(fadeIn, 1);
 		$('.nav-indoor-duration').css('color', 'black');
 		$('title').html("Indoor Duration | Free Flight");
 	});
 	$('.nav-olympiad, .nav-olympiad-wright-stuff, .nav-olympiad-elastic-launched-glider').click(function() {
 		resetNav();
-		$('.olympiad').css('display', 'block');
+		$('.olympiad').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'block'); }).fadeTo(fadeIn, 1);
 		$('.nav-olympiad').css('color', 'black');
 		$('title').html("Science Olympiad | Free Flight");
 	});
 	$('.nav-utilities, .nav-utilities-torque, .nav-utilities-balsa-density, .nav-utilities-dial').click(function() {
 		resetNav();
-		$('.utilities').css('display', 'block');
+		$('.utilities').fadeTo(fadeOut, 0, function(){ $(this).css('display', 'block'); }).fadeTo(fadeIn, 1);
 		$('.nav-utilities').css('color', 'black');
 		$('title').html("Utilities | Free Flight");
 	});
